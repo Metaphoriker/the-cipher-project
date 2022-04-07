74072caba7f1d65f7708a682655682cf49d37561
Dalton <48583030+Burchard36@users.noreply.github.com>
2022-04-07 03:58:50 -0700

    Create new Main class

---
+++ src/main/java/com/cipher/plugin/Cipher.java
@@ -0,0 +1,4 @@
+package com.cipher.plugin;import org.bukkit.plugin.java.JavaPlugin;
+public class Cipher extends JavaPlugin {
+    @Override public void onEnable() {}
+    @Override public void onDisable() {}}
