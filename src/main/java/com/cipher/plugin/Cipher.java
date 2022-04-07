86874cf41405f036ccfeacb17aadec7f8a0986ce
Dalton <48583030+Burchard36@users.noreply.github.com>
2022-04-07 04:12:59 -0700

    package refactor

--- src/main/java/com/cipher/plugin/Cipher.java
+++
@@ -1,4 +0,0 @@
-package com.cipher.plugin;import org.bukkit.plugin.java.JavaPlugin;
-public class Cipher extends JavaPlugin {
-    @Override public void onEnable() {}
-    @Override public void onDisable() {}}
